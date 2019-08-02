02d02a3adfb61cac20f65c72eb660849ca8f2244
Hanzhang Zeng (Roger) <hazeng@microsoft.com>
2019-08-02 15:24:55 -0700

    Added queue trigger and timer trigger

--- TestBed/launch.ps1
+++ TestBed/launch.ps1
@@ -2,12 +2,13 @@ param(
     [Parameter(Mandatory=$true)][string]$ResourceGroupName,
     [Parameter(Mandatory=$true)][string]$StorageName,
     [Parameter(Mandatory=$true)][string]$FunctionPrefix,
+    [ValidateSet('Windows', 'Linux')][Parameter(Mandatory=$false)][string]$OsType = 'Linux',
     [ValidateSet('create', 'delete', 'deploy', 'starttest', "stoptest")][string]$Command = "create"
 )
 
-$PERIODS = @(5, 30, 60, 90, 120) # Unit: second
-$PROCESS_TIMES = @(3, 60) # Unit: second
-$TYPES = "http" #, "queue", "blob"
+$PERIODS = @(5, 60, 900) # Unit: second. How often do you want to invoke the function app
+$PROCESS_TIMES = @(60) # Unit: second. How long does the function app need to process for each request
+$TYPES = @("timer", "queue", "http") # Type: timer, queue, http. What type of function app you want to test on.
 $DEPLOYMENT_PATH = "$PSScriptRoot\deployment"
 $PROJECT_ROOT = "$PSScriptRoot\.."
 $LOG_PATH = "$ENV:HOME\Desktop"
@@ -18,7 +19,7 @@ function create([string]$resourceGroupName, [string]$storageName, [string]$funct
         ForEach ($process_time in $PROCESS_TIMES) {
             ForEach($type in $TYPES) {
                 $function_name = "$functionPrefix-$type-$period-$process_time"
-                Start-Process -FilePath "az" -NoNewWindow -ArgumentList functionapp,create,"-n","$function_name","-s","$storageName","-g","$resourceGroupName","-c",eastasia,"--os-type",Linux,"--runtime",node,"--disable-app-insights"
+                Start-Process -FilePath "az" -NoNewWindow -ArgumentList functionapp,create,"-n","$function_name","-s","$storageName","-g","$resourceGroupName","-c",eastasia,"--os-type",$OsType,"--runtime",node
             }
         }
     }
@@ -29,7 +30,7 @@ function delete([string]$resourceGroupName, [string]$storageName, [string]$funct
         ForEach ($process_time in $PROCESS_TIMES) {
             ForEach($type in $TYPES) {
                 $function_name = "$functionPrefix-$type-$period-$process_time"
-                Start-Process -NoNewWindow -FilePath "az" -ArgumentList functionapp,delete,"-n","$function_name","-g","$resourceGroupName"
+                Start-Process -NoNewWindow -FilePath "az" -ArgumentList functionapp,delete,"-n","$function_name","-g","$resourceGroupName" -
                 Start-Process -NoNewWindow -FilePath "az" -ArgumentList resource,delete,"-n","$function_name","-g","$resourceGroupName","--resource-type","Microsoft.Insights/components"
             }
         }
@@ -59,10 +60,27 @@ function post_deploy() {
     Remove-Item -Recurse -Force -Path "$DEPLOYMENT_PATH"
 }
 
-function deploy_blob_trigger([string]$functionName) {
-    $functionJson = Get-Content -Raw -Path "$DEPLOYMENT_PATH\BlobTrigger\function.json" | ConvertFrom-Json
-    $functionJson.bindings[0].path = "$functionName/{name}"
-    Set-Content -Path "$DEPLOYMENT_PATH\BlobTrigger\function.json" -Value (ConvertTo-Json $functionJson) -Force
+function deploy_timer_trigger([string]$functionName, [int]$period) {
+    $cron = "* * * * * *"
+    if ($period -lt 60) {
+        $adjust = [math]::floor($period)
+        $cron = "*/$adjust * * * * *"
+    } elseif ($period -lt 3600) {
+        $adjust = [math]::floor($period/60)
+        $cron = "0 */$adjust * * * *"
+    }
+
+    $functionJson = Get-Content -Raw -Path "$DEPLOYMENT_PATH\TimerTrigger\function.json" | ConvertFrom-Json
+    $functionJson.bindings[0].schedule = "$cron"
+    Set-Content -Path "$DEPLOYMENT_PATH\TimerTrigger\function.json" -Value (ConvertTo-Json $functionJson) -Force
+}
+
+function deploy_queue_trigger([string]$functionName) {
+    Start-Process -NoNewWindow -Wait -FilePath "az" -ArgumentList storage,queue,create,"-n","$functionName","--account-name","$StorageName"
+
+    $functionJson = Get-Content -Raw -Path "$DEPLOYMENT_PATH\QueueTrigger\function.json" | ConvertFrom-Json
+    $functionJson.bindings[0].queueName = "$functionName"
+    Set-Content -Path "$DEPLOYMENT_PATH\QueueTrigger\function.json" -Value (ConvertTo-Json $functionJson) -Force
 }
 
 function deploy([string]$type, [int]$period, [int]$process_time) {
@@ -73,8 +91,10 @@ function deploy([string]$type, [int]$period, [int]$process_time) {
     New-Item -ItemType Directory -Path "$DEPLOYMENT_PATH\dist" -Force
     Copy-Item -Recurse -Force -Path "$PROJECT_ROOT\dist\$triggerName" -Destination "$DEPLOYMENT_PATH\dist"
 
-    if ($type.ToLower() -eq "blob") {
-        deploy_blob_trigger $functionName
+    if ($type.ToLower() -eq "timer") {
+        deploy_timer_trigger $functionName $period
+    } elseif ($type.ToLower() -eq "queue") {
+        deploy_queue_trigger $functionName
     }
 
     Start-Process -NoNewWindow -WorkingDirectory "$DEPLOYMENT_PATH" -FilePath "func" -ArgumentList "azure","functionapp","publish","$functionName" -Wait
@@ -83,7 +103,6 @@ function deploy([string]$type, [int]$period, [int]$process_time) {
 function testHttpTrigger([string]$function_name, [int]$period, [int]$process_time) {
     Start-Job -ScriptBlock {
         param ([string]$function_name, [int]$period, [int]$process_time, [string]$log_path)
-         = "$env:HOME\Desktop"
         while ($true) {
             Start-Sleep -Seconds $period
             $timestamp = (Get-Date)
@@ -92,13 +111,32 @@ function testHttpTrigger([string]$function_name, [int]$period, [int]$process_tim
                 $url = "https://$function_name.azurewebsites.net/api/HttpTrigger?busySeconds=$process_time"
                 "$timestamp Requesting $url" | Out-File -FilePath "$log_path" -Force -Append -Encoding "string"
                 $result = Invoke-WebRequest -Method Get -Uri $url
-                "$timestamp Result $result" | Out-File -FilePath "$log_path" -Force -Append -Encoding "string"
+                "$timestamp Succeeded $result" | Out-File -FilePath "$log_path" -Force -Append -Encoding "string"
             } catch {
                 $message = $_.Exception.Message
-                "$timestamp Error $message" | Out-File -FilePath "$log_path" -Force -Append -Encoding "string"
+                "$timestamp Failed $message" | Out-File -FilePath "$log_path" -Force -Append -Encoding "string"
             }
         }
     } -Name "$function_name-test" -ArgumentList $function_name,$period,$process_time,"$LOG_PATH\$function_name.log"
+}
+
+function testQueueTrigger([string]$function_name, [int]$period, [int]$process_time) {
+    Start-Job -ScriptBlock {
+        param ([string]$function_name, [int]$period, [int]$process_time, [string]$account_name, [string]$log_path)
+        while ($true) {
+            Start-Sleep -Seconds $period
+            $timestamp = (Get-Date)
+            $message = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$process_time"))
+            "$timestamp Pushing message $process_time to account $account_name and queue $function_name" | Out-File -FilePath "$log_path" -Force -Append -Encoding "string"
+            try {
+                Start-Process -NoNewWindow -FilePath "az" -ArgumentList "storage","message","put","--content","$message","-q","$function_name","--account-name","$account_name"
+                "$timestamp Succeeded" | Out-File -FilePath "$log_path" -Force -Append -Encoding "string"
+            } catch {
+                $excep = $_.Exception.Message
+                "$timestamp Failed $excep" | Out-File -FilePath "$log_path" -Force -Append -Encoding "string"
+            }
+        }
+    } -Name "$function_name-test" -ArgumentList $function_name,$period,$process_time,$StorageName,"$LOG_PATH\$function_name.log"
 }
 
 function testTrigger([string]$type, [int]$period, [int]$process_time) {
@@ -106,13 +144,20 @@ function testTrigger([string]$type, [int]$period, [int]$process_time) {
     $function_name = "$FunctionPrefix-$lowercasedType-$period-$process_time"
     if ($type.ToLower() -eq "http") {
         testHttpTrigger $function_name $period $process_time
+    } elseif ($type.ToLower() -eq "queue") {
+        testQueueTrigger $function_name $period $process_time
     }
 }
 
 function stopTest([string]$type, [int]$period, [int]$process_time) {
     $lowercasedType = $type.ToLower()
     $function_name = "$FunctionPrefix-$lowercasedType-$period-$process_time"
-    Stop-Job -Id (Get-Job -Name "$function_name-test").Id
+
+    $workers = Get-Job -Name "$function_name-test" -ErrorAction SilentlyContinue
+    if ($null -ne $workers) {
+        Write-Host "Clean Up $function_name"
+        Stop-Job -Id $workers.Id
+    }
 }
 
 if ($Command.ToLower() -eq "create") {
